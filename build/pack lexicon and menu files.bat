d0c4aa9bc1b85e45f0471d53f9067278aefdb960
Ahrkylien <henkvgrootheest@gmail.com>
2025-11-15 19:08:16 +0100

    improve (un)packing

---
+++ build/pack lexicon and menu files.bat
@@ -0,0 +1,2 @@
+python3.10 "pack lexicon and menu files.py"
+pause
---
+++ build/pack lexicon and menu files.py
@@ -0,0 +1,3 @@
+from BF1942_Extraction_Readout_Scripts.mod_utilities import pack_lexicon_and_menu_files
+
+pack_lexicon_and_menu_files("../src", "GCMOD")
