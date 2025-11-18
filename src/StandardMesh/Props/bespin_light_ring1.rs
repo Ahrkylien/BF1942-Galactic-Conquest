9cbe8b6592cf8b3e74d742867829b821102cfa70
Ahrkylien <henkvgrootheest@gmail.com>
2025-11-19 00:52:03 +0100

    move bespin light ring to shared archives

--- src/StandardMesh/Props/bespin_light_ring1.rs
+++ src/StandardMesh/Props/bespin_light_ring1.rs
@@ -1,7 +1,7 @@
 subshader "material_0" "StandardMesh/Default"
 {
 	lighting false;
-	texture "texture/Buildings/Bespin/bespin_light1_center";
+	texture "texture/Props/bespin_light_center";
 	materialDiffuse 1.0 1.0 1.0;
 	materialSpecular 1.0 1.0 1.0;
 	materialSpecularPower 12.5;
