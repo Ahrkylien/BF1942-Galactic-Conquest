46a22d8d9e9037de2307b580e74adaada10bcd5c
Ahrkylien <henkvgrootheest@gmail.com>
2025-12-13 00:02:19 +0100

    cleanup mos entha shared files

--- src/bf1942/levels/GC_Mos_Entha/StandardMesh/Buildings/Tatooine/bridge1stairs_m1.rs
+++
@@ -1,8 +0,0 @@
-subshader "bridge1stairs_m1_Material0" "StandardMesh/Default"
-{
-	lighting true;
-	materialDiffuse 1 1 1;
-	lightingSpecular false;
-	texture "texture/Buildings/Tatooine/bridge1";
-}
-
