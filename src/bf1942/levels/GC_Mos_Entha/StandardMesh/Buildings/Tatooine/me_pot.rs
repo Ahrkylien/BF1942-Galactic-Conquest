365b9e390c08387e6a58d2756f8e6731ad220cfb
Ahrkylien <henkvgrootheest@gmail.com>
2025-12-12 23:38:45 +0100

    remove duplicates

--- src/bf1942/levels/GC_Mos_Entha/StandardMesh/Buildings/Tatooine/me_pot.rs
+++
@@ -1,8 +0,0 @@
-subshader "me_pot_Material0" "StandardMesh/Default"
-{
-	lighting true;
-	materialDiffuse 1 1 1;
-	lightingSpecular false;
-	texture "texture/Buildings/Tatooine/me_pot";
-}
-
--- src/bf1942/levels/GC_Mos_Entha/StandardMesh/Buildings/Tatooine/tat_ringthing_m1.rs
+++
@@ -1,8 +0,0 @@
-subshader "tat_ringthing_m1_Material0" "StandardMesh/Default"
-{
-	lighting true;
-	materialDiffuse 1 1 1;
-	lightingSpecular false;
-	texture "texture/Buildings/Tatooine/tat_wall_stuco_door";
-}
-
