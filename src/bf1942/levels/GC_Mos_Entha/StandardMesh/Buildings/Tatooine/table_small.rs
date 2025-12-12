dfdae7fa4f348cb09053822ee7abd320eeddb2ed
Ahrkylien <henkvgrootheest@gmail.com>
2025-12-12 23:38:45 +0100

    remove duplicates

--- src/bf1942/levels/GC_Mos_Entha/StandardMesh/Buildings/Tatooine/table_small.rs
+++
@@ -1,8 +0,0 @@
-subshader "table_small_Material0" "StandardMesh/Default"
-{
-	lighting true;
-	materialDiffuse 1 1 1;
-	lightingSpecular false;
-	texture "texture/Buildings/Tatooine/table";
-}
-
--- src/bf1942/levels/GC_Mos_Entha/StandardMesh/Buildings/Tatooine/tat_floorbed_m1.rs
+++
@@ -1,8 +0,0 @@
-subshader "tat_floorbed_m1_Material0" "StandardMesh/Default"
-{
-	lighting true;
-	materialDiffuse 1 1 1;
-	lightingSpecular false;
-	texture "texture/Buildings/Tatooine/me_floorbed";
-}
-
