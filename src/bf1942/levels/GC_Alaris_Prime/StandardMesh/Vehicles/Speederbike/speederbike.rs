ff138fed0af70499a509a0c3ab217122aae982ea
Ahrkylien <henkvgrootheest@gmail.com>
2025-12-13 17:27:01 +0100

    remove duplicate speederbike

--- src/bf1942/levels/GC_Alaris_Prime/StandardMesh/Vehicles/Speederbike/speederbike.rs
+++
@@ -1,8 +0,0 @@
-subshader "speederbike_Material0" "StandardMesh/Default"
-{
-	lighting true;
-	lightingSpecular false;
-	materialDiffuse 1.0 1.0 1.0;
-	texture "texture/Vehicles/Speederbike/Speederbike";
-}
-
--- src/bf1942/levels/GC_Alaris_Prime/StandardMesh/Vehicles/Speederbike/speederbike_bb.rs
+++
@@ -1,8 +0,0 @@
-subshader "speederbike_bb_Material0" "StandardMesh/Default"
-{
-	lighting true;
-	lightingSpecular false;
-	materialDiffuse 0.588 0.588 0.588;
-	texture "texture/notexture";
-}
-
