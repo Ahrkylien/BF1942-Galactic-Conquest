791c85ddb12fdf428f96c57dc1eeca6618bb84a4
Ahrkylien <henkvgrootheest@gmail.com>
2025-12-13 11:28:49 +0100

    clean up mos entha

--- src/bf1942/levels/GC_Mos_Entha/StandardMesh/Buildings/Tatooine/gcsupply.rs
+++
@@ -1,7 +0,0 @@
-subshader "gcsupply_Material0" "StandardMesh/Default"
-{
-	lighting true;
-	materialDiffuse 1.0 1.0 1.0;
-	lightingSpecular false;
-	texture "texture/Buildings/Tatooine/tat_stucco1";
-}
